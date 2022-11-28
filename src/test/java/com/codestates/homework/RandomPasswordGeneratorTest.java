892ed6e5d889b4d4f9f7f7a0dd09dc10476d72e2
hjs6877 <hjs6877@gmail.com>
2022-11-28 11:53:34 +0900

    Remove length attribute for Coffee Entity

--- src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
+++ src/test/java/com/codestates/homework/RandomPasswordGeneratorTest.java
@@ -1,11 +1,7 @@
 package com.codestates.homework;
 
-import com.codestates.helper.RandomPasswordGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 
 public class RandomPasswordGeneratorTest {
     @DisplayName("실습 3: 랜덤 패스워드 생성 테스트")
